3a36355900d023ceef16c9e661ab33c350956271
Delvin <write2delvin@gmail.com>
2023-11-02 20:51:30 +1100

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3001
 
-app.get("/", (req, res)=> req.json("This the forst one"));
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
